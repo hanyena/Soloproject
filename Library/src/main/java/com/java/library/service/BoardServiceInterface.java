b8a9d71093e90c1eefc6017178756ad6139fa6ce
Yoo <Yoo@HomeComputer>
2018-01-15 01:12:19 +0900

    a

--- Library/src/main/java/com/java/library/service/BoardServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.java.library.service;
-
-import java.util.HashMap;
-
-public interface BoardServiceInterface {
-	
-	public HashMap<String, Object> BoardSelect(HashMap<String, Object> paramMap);
-
-}
