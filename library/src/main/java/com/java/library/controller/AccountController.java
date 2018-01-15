c5a75bb2df337b5be0cb96cc9057a0eb26dcdccf
Yoo <Yoo@HomeComputer>
2018-01-16 01:16:04 +0900

    계정 관련 추가 .

--- library/src/main/java/com/java/library/controller/AccountController.java
+++ library/src/main/java/com/java/library/controller/AccountController.java
@@ -3,6 +3,9 @@ package com.java.library.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.java.library.service.MemberServiceInterface;
@@ -36,19 +40,26 @@ public class AccountController{
 		paramMap.put("id", "yena");
 		paramMap.put("password", "yena456");
 		
-		
 		session.setAttribute("UserSession", msi.getMember(paramMap));
 		String plainString = JSONObject.fromObject(JSONSerializer.toJSON(session.getAttribute("UserSession"))).toString();
 		String convertString = plainString.trim();
 		System.out.println(plainString);
 		System.out.println(convertString);
-		
-		return ConverterUtil.prettyJSON(convertString);
+		return plainString;
 	}
-	
 	@RequestMapping(value="login.do",method=RequestMethod.POST) 
 	public String loginProcess(@RequestParam Map<String,Object> paramMap) {
 		
 		return "";
+	}
+	
+	@RequestMapping(value="popup/login.do",method=RequestMethod.GET) 
+	public ModelAndView popupLogin(ModelAndView mav,@RequestParam Map<String,Object> paramMap,HttpServletRequest req, HttpServletResponse res) {
+		
+		mav.setViewName("/account/popup/account_login");
+		mav.addObject("port",req.getRemotePort());
+		mav.addObject("hostname",req.getRemoteHost());
+		mav.addObject("addr",req.getRemoteAddr());
+		return mav;
 	}
 }
